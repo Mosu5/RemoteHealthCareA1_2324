e53975e5bb2e8413de7bee27a32200a27e0045b2
m05u <m.azaimi@student.avans.nl>
2023-09-01 11:02:52 +0200

    Update Program.cs
    Improved function to calculate checksum in payload

--- FietsDemo/FietsDemo/Program.cs
+++ FietsDemo/FietsDemo/Program.cs
@@ -17,6 +17,7 @@ namespace FietsDemo
             BLE bleHeart = new BLE();
             Thread.Sleep(1000); // We need some time to list available devices
 
+
             // List available devices
             List<String> bleBikeList = bleBike.ListDevices();
             Console.WriteLine("Devices found: ");
@@ -37,11 +38,28 @@ namespace FietsDemo
 
             // Set service
             errorCode = await bleBike.SetService("6e40fec1-b5a3-f393-e0a9-e50e24dcca9e");
+
             // __TODO__ error check
 
             // Subscribe
             bleBike.SubscriptionValueChanged += BleBike_SubscriptionValueChanged;
             errorCode = await bleBike.SubscribeToCharacteristic("6e40fec2-b5a3-f393-e0a9-e50e24dcca9e");
+
+            // Switch to data page 11
+            byte pageRequested = 0x11;
+
+            byte[] requestData = new byte[13];
+            requestData[0] = (byte)0xA4;
+            requestData[1] = (byte)0x09;
+            requestData[2] = (byte)0x4E;
+            requestData[3] = (byte)0x46;
+            requestData[9] = (byte) 0x80;
+            requestData[10] = (byte)0x19;
+            requestData[11] = (byte)0x01;
+
+            byte checkSum = ComputeAdditionChecksum(requestData);
+            requestData[12] = checkSum;
+            bleBike.WriteCharacteristic("6e40fec2-b5a3-f393-e0a9-e50e24dcca9e", requestData);
 
             // Heart rate
             errorCode =  await bleHeart.OpenDevice("Decathlon Dual HR");
@@ -69,6 +87,7 @@ namespace FietsDemo
             String hexString = BitConverter.ToString(ba).ToLower().Replace("-", " 0x");
             StringBuilder sb = new StringBuilder();
             sb.Append("0x");
+            
             sb.Append(hexString);
 
             return sb.ToString();
@@ -80,8 +99,15 @@ namespace FietsDemo
             if (ba[3].Equals(0x05)) {
                 // convert byte to values
                 int speed = Convert.ToInt16(ba[4]);
+                
             }
             return sb.ToString();
+        }
+
+        public static byte ComputeAdditionChecksum(byte[] data)
+        {
+            long longSum = data.Sum(x => (long)x);
+            return unchecked((byte)longSum);
         }
     }
 }
