301375c298b819e70870946f217eeb5c71478026
m05u <m.azaimi@student.avans.nl>
2023-09-05 18:31:00 +0200

    Added temp bike code
    Partial working code for reading data from the bike using the FietsDemo project

---
+++ FietsDemo/FietsDemo/BLE/Test.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Test
+{
+     static async Task main(string[] args)
+    {
+        IBike bleBike = new BLEBike();
+        DataHandler dataHandler = new DataHandler(bleBike);
+    }
+}
---
+++ ProjectRemoteHealthCare/BLE/BLEBike.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class BLEBike : Bike
+{
+	public BLEBike()
+	{
+	}
+}
