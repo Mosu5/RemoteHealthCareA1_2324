101a4546c5203b05e4e604427c3b7f094f8d4582
JaidyMullens <jaidymullens12@gmail.com>
2023-09-07 14:19:30 +0200

    Added SetResistance

--- HealthCareApplication/BikeConnection/Program.cs
+++ HealthCareApplication/BikeConnection/Program.cs
@@ -11,11 +11,19 @@ namespace FietsDemo
 {
     class Program
     {
+         
+
+        //bool readData = true;
+        //bool readOther = false;
         static async Task Main(string[] args)
         {
+            BLE bleBike;
+          BLE bleHeart;
+        bleBike = new BLE();
+            bleHeart = new BLE();
+
             int errorCode = 0;
-            BLE bleBike = new BLE();
-            BLE bleHeart = new BLE();
+
             Thread.Sleep(1000); // We need some time to list available devices
 
             // List available devices
@@ -27,7 +35,7 @@ namespace FietsDemo
             }
 
             // Connecting
-            errorCode = errorCode = await bleBike.OpenDevice("Tacx Flux 01140");
+            errorCode = errorCode = await bleBike.OpenDevice("Tacx Flux 01249");
             // __TODO__ Error check
 
             var services = bleBike.GetServices;
@@ -53,11 +61,43 @@ namespace FietsDemo
             await bleHeart.SubscribeToCharacteristic("HeartRateMeasurement");
 
 
+
             Console.Read();
         }
 
+        /// </summary>
+        /// <param name="resistance">0-255</param>
+        /// <returns>
+        /// The resistance is being returned.
+        /// </returns>
+        //public void SetResistanceAsync(int resistance)
+        //{
+        //    if (bleBike == null)
+        //        return;
+        //    byte[] output = new byte[13];
+        //    output[0] = 0x4A; //sync byte
+        //    output[1] = 0x09; //Message Length
+        //    output[2] = 0x4E; //Message type
+        //    output[3] = 0x05; //Message type
+        //    output[4] = 0x30; //Datatype
+        //    output[11] = (byte)resistance;
+        //    byte checksum = output[0];
+        //    for (int i = 1; i < 12; i++)
+        //    {
+        //        checksum ^= output[i];
+        //    }
+        //    output[12] = checksum;
+        //    bleBike.WriteCharacteristic("6e40fec3-b5a3-f393-e0a9-e50e24dcca9e", output);
+        //}
+
+
+
+        // General method to handle everything regarding data changed
         private static void BleBike_SubscriptionValueChanged(object sender, BLESubscriptionValueChangedEventArgs e)
         {
+
+            
+          
             byte[] receivedData = e.Data;
 
             bool ifVerified = VerifyMessage(receivedData);
