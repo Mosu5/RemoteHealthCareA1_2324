6027c7fa0779a9b2ee5831dfb3933dd0e5083ab4
JaidyMullens <jaidymullens12@gmail.com>
2023-09-14 14:03:37 +0200

    Connection to VR Server

---
+++ HealthCareApplication/VRConnection/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace VRConnection
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            // Application to test connection to VR server
+
+            // Initialize Server connection
+            TcpClient tcpClient = new TcpClient("145.48.6.10", 6666);
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.Write("Write message: ");
+                string message = Console.ReadLine();
+                if (message.Equals("quit"))
+                {
+                    running = false;
+                }
+
+                TunnelHandler.SendMessage(networkStream, message);
+
+                string response = TunnelHandler.ReadMessage(networkStream);
+                Console.WriteLine("Response:" + response);
+
+            }
+            tcpClient.Close();
+            networkStream.Close();
+
+
+
+        }
+
+    }
+
+
+}
---
+++ HealthCareApplication/VRConnection/TunnelHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRConnection
+{
+    public class TunnelHandler
+    {
+
+        //TODO change to take JSON message
+        public static string ReadMessage(NetworkStream networkStream)
+        {
+
+            byte[] lenghtArray = new byte[4];
+
+            networkStream.Read(lenghtArray, 0, 4);
+            uint lenght = BitConverter.ToUInt32(lenghtArray, 0);
+
+            Console.WriteLine(lenght);
+
+            byte[] buffer = new byte[lenght];
+            int totalRead = 0;
+
+            // Read bytes untill no more
+            while(networkStream.DataAvailable)
+            {
+                int read = networkStream.Read(buffer, totalRead, buffer.Length - totalRead);
+                totalRead += read;
+                Console.WriteLine("ReadMessage: " + read);
+            }
+
+            return Encoding.ASCII.GetString(buffer, 0, totalRead);
+        }
+
+
+        //TODO change to take JSON message
+        public static void SendMessage(NetworkStream networkStream, object payload)
+        {
+            // Transforming the object to a string to be able to send the data
+            string message = payload.ToString();
+
+            //make sure the other end decodes with the same format!
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+            byte[] length = BitConverter.GetBytes((uint)message.Length); // Getting the length of the payload
+            Console.WriteLine("Lenghts = {0}", message.Length);
+
+            // Takes the first four bytes that indicate lenght and adds it to the total load
+            bytes = length.Concat(bytes).ToArray();
+
+            networkStream.Write(bytes, 0, bytes.Length);
+        }
+
+    }
+}
