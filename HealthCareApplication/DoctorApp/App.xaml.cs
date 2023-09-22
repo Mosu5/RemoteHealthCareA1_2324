a5e8ac102c7536e4166cd70c3efa67998f73a3ec
Moustapha Azaïmi <m.azaimi@student.avans.nl>
2023-09-22 23:28:12 +0200

    Create Doctor, Server, Patient projects

---
+++ HealthCareApplication/DoctorApp/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DoctorApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ HealthCareApplication/PatientApp/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace PatientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
