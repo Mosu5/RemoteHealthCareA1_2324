cee33374e64d1ac6bd7d9e111fcca94fa3b014c9
Simon <saj.decock@student.avans.nl>
2023-09-24 13:43:01 +0200

    Implement Summary command and add command input functionality

--- HealthCareApplication/PatientApp/Commands/Summary.cs
+++ HealthCareApplication/PatientApp/Commands/Summary.cs
@@ -9,9 +9,42 @@ namespace RHSandbox.Commands
 {
     internal class Summary : ISessionCommand
     {
+        /// <summary>
+        /// Currently prints out a summary of all statistics that were received by the server.
+        /// </summary>
         public bool Execute(JsonObject data)
         {
-            throw new NotImplementedException();
+            JsonArray dataArray = data.AsArray();
+            StringBuilder sb = new StringBuilder($"==== SUMMARY OF STATISTICS ====\n\tAmount:\t{dataArray.Count} statistics\n");
+
+            // Loop through array of the objects containing the statistics
+            foreach (var statAsNode in dataArray)
+            {
+                JsonObject statAsObject = statAsNode.AsObject();
+                if (!IsStatValid(statAsObject)) continue;
+
+                int index = dataArray.IndexOf(statAsNode);
+
+                // Append statistics as string
+                sb.Append($"\n{index}.\n");
+                sb.Append($"\tTime:\t{statAsObject["time"]}\n\tSpeed:\t{statAsObject["speed"]}\n\tDistance:\t{statAsObject["distance"]}\n\tHeart rate:\t{statAsObject["heartrate"]}\n");
+            }
+
+            sb.Append("==== END SUMMARY OF STATISTICS ====");
+            Console.WriteLine(sb.ToString());
+
+            return true;
+        }
+
+        /// <summary>
+        /// Wether the given statistics object contains the correct keys
+        /// </summary>
+        private bool IsStatValid(JsonObject stat)
+        {
+            return stat.ContainsKey("time") &&
+                stat.ContainsKey("speed") &&
+                stat.ContainsKey("distance") &&
+                stat.ContainsKey("heartrate");
         }
     }
 }
--- HealthCareApplication/PatientApp/Program.cs
+++ HealthCareApplication/PatientApp/Program.cs
@@ -1,20 +1,23 @@
 ﻿using RHSandbox.Commands;
 using System;
+using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Utilities.Communication;
 
 namespace PatientApp
 {
     internal class Program
     {
-        private static ISessionCommand _command;
+        private static ISessionCommand _command = new Login();
 
         static async Task Main(string[] args)
         {
             try
             {
-                if (await DataTransfer.ConnectToServer("127.0.0.1", 6969))
+                //if (await DataTransfer.ConnectToServer("127.0.0.1", 6969))
                     await Run();
             }
             catch (CommunicationException ex)
@@ -25,9 +28,9 @@ namespace PatientApp
 
         private static async Task Run()
         {
-            // Initialize command to be login
-            _command = new Login();
-            _command.Execute(null);
+            // Run a thread that listens for console input
+            Thread t = new Thread(ReceiveConsoleInput);
+            t.Start();
 
             // Declare a variable inside while condition which listens for inbound JSON messages.
             // Loop blocks until a new message is received.
@@ -55,10 +58,64 @@ namespace PatientApp
                         _command = new SendStats();
                         break;
                     default:
-                        _command = null;
                         throw new CommunicationException("Unknown command received: " + commandField);
                 }
                 _command?.Execute(dataField);
+            }
+        }
+
+        /// <summary>
+        /// Receives string commands from the console input and applies the command if valid.
+        /// </summary>
+        private static void ReceiveConsoleInput()
+        {
+            while (true)
+            {
+                object payload;
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "login":
+                        // Receive username and password
+                        Console.Write("Username: ");
+                        string username = Console.ReadLine();
+                        Console.Write("Password: ");
+                        string password = Console.ReadLine();
+                        payload = new { username, password };
+
+                        ApplyCommand(new Login(), payload);
+                        break;
+                    case "stats/send":
+                        // Receive speed, distance and heart rate
+                        Console.Write("Speed: ");
+                        int speed = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Distance: ");
+                        int distance = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Heart rate: ");
+                        int heartrate = Convert.ToInt32(Console.ReadLine());
+                        payload = new { speed, distance, heartrate };
+
+                        ApplyCommand(new SendStats(), payload);
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command " + input);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Accesses the _command variable in a thread-safe manner and executes the given command.
+        /// </summary>
+        private static void ApplyCommand(ISessionCommand command, object payload)
+        {
+            // Lock the _command variable, so no other thread can access it while the program is inside this lock block.
+            lock (_command)
+            {
+                _command = command;
+                // Please tell me there's a better way to cast an object to JsonObject
+                _command.Execute(JsonSerializer.Deserialize<JsonObject>(JsonSerializer.Serialize(payload)));
             }
         }
     }
