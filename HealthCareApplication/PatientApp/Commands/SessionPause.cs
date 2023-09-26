e044c88064a77c1f702bde14114684c23e335446
Simon <saj.decock@student.avans.nl>
2023-09-26 15:11:28 +0200

    Work on session states

--- HealthCareApplication/PatientApp/Commands/SessionPause.cs
+++ HealthCareApplication/PatientApp/Commands/SessionPause.cs
@@ -5,9 +5,23 @@ namespace PatientApp.Commands
 {
     internal class SessionPause : ISessionCommand
     {
+        //private readonly IReceiver receiver;
+        //private readonly EventHandler<object> _onReceiveData;
+
+        //public SessionStart(IReceiver receiver, EventHandler<object> onReceiveData)
+        //{
+        //    this.receiver = receiver;
+        //    _onReceiveData = onReceiveData;
+        //}
+
         public bool Execute(JsonObject data)
         {
-            throw new NotImplementedException();
+            //// Subscribe to all bike and HRM events.
+            //receiver.ReceivedSpeed -= _onReceiveData;
+            //receiver.ReceivedDistance -= _onReceiveData;
+            //receiver.ReceivedHeartRate -= _onReceiveData;
+            //receiver.ReceivedRrIntervals -= _onReceiveData;
+            return true;
         }
     }
 }
