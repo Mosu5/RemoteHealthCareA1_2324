040f6fe35146d27496af4facf5349b0806ae1d61
Moustapha Azaïmi <m.azaimi@student.avans.nl>
2023-10-05 14:22:41 +0200

    add wpf project for patient client

---
+++ HealthCareApplication/PatientWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PatientWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
