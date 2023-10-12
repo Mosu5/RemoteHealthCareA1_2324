af53baf934517688f35a2aa8152abdb2d198d408
KoenPothof <koenpothof@gmail.com>
2023-10-12 13:13:07 +0200

    Add stat window for doctor

---
+++ HealthCareApplication/DoctorWPFApp/View/StatsD.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace DoctorWPFApp.View
+{
+  
+    public partial class StatsD : Window
+    {
+        public StatsD()
+        {
+            InitializeComponent();
+        }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SessionSettingD sessionSetting = new SessionSettingD();
+            Close();
+            sessionSetting.Show();
+        }
+
+        private void summaryBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Averege speed: " + "\n" + "Distance: " + "\n" + "Average heartrate: ", "Summary so far!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
