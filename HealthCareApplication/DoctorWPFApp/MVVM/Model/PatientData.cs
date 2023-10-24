6a28f25b06f7ea0bab81d11162d32cf019f1d22d
Moustapha Azaïmi <m.azaimi@student.avans.nl>
2023-10-24 20:27:09 +0200

    Fixed listview in StatsWindowD and patientdata model

---
+++ HealthCareApplication/DoctorWPFApp/MVVM/Model/PatientData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DoctorWPFApp.MVVM.Model
+{
+    internal class PatientData
+    {
+        public DateTime DateTime { get; set; } = DateTime.MinValue;
+        public double RecordedSpeed { get; set; } = 0;
+        public double RecordedDistance { get; set; } = 0;
+        public double RecordedHeartRate { get; set; } = 0;
+        public double RecordedRrInterval { get; set; } = 0;
+    }
+}
