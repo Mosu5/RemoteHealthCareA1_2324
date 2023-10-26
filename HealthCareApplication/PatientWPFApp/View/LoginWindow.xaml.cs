70f3f2a45e8a5bdf2a7f0670950b597970921103
Simon <saj.decock@student.avans.nl>
2023-10-26 21:18:10 +0200

    Set up neater UI view

---
+++ HealthCareApplication/PatientWPFApp/View/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PatientWPFApp.View
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameInput.Text;
+            string password = PasswordInput.Password;
+
+            SessionWindow sessionWindow = new();
+            sessionWindow.Show();
+            Close();
+        }
+    }
+}
---
+++ HealthCareApplication/PatientWPFApp/View/SessionWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PatientWPFApp.View
+{
+    /// <summary>
+    /// Interaction logic for SessionWindow.xaml
+    /// </summary>
+    public partial class SessionWindow : Window
+    {
+        public SessionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleSessionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EmergencyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SetResistanceButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StopExitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SendChatButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
